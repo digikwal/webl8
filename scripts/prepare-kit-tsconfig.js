d93cd98fb2d8c7a62a67b9f459189aec8f483293
digikwal <digikwal@users.noreply.github.com>
2025-06-09 01:43:35 +0200

    feat: add script to create fallback tsconfig.json for SvelteKit

---
+++ scripts/prepare-kit-tsconfig.js
@@ -0,0 +1,11 @@
+// scripts/prepare-kit-tsconfig.js
+import fs from 'fs';
+const fallbackPath = './tsconfig.json';
+
+if (!fs.existsSync('./.svelte-kit/tsconfig.json')) {
+    const fallback = {
+        extends: "@tsconfig/svelte/tsconfig.json",
+        include: ["src"]
+    };
+    fs.writeFileSync(fallbackPath, JSON.stringify(fallback, null, 2));
+}

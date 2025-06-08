a0652807646fd3f1f234edf08567e07a337fdde3
digikwal <digikwal@users.noreply.github.com>
2025-06-08 16:24:57 +0200

    feat: Add TypeScript and Vite configuration files; enhance project setup with linting and formatting scripts

---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+      pages: 'dist',
+      assets: 'dist',
+      fallback: null,
+      precompress: false
+    }),
+    paths: {
+      base: '/webl8'
+    }
+  },
+  preprocess: vitePreprocess()
+};
+
+export default config;
